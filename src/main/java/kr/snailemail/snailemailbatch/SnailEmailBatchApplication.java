f23adfe62890db1c0acd29d2b77c3a626f15b2ff
serinheo <serin9811@gmail.com>
2022-11-17 21:23:34 +0900

    init

---
+++ src/main/java/kr/snailemail/snailemailbatch/SnailEmailBatchApplication.java
@@ -0,0 +1,13 @@
+package kr.snailemail.snailemailbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailEmailBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnailEmailBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/snailemail/snailemailbatch/SnailEmailBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.snailemail.snailemailbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnailEmailBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
