eda7f9a7ac5fa1868aa8d70e2ba17497c6e8d7bb
serinheo <serin9811@gmail.com>
2022-11-20 21:49:28 +0900

    add: add batch config and scheduler

--- src/main/java/kr/snailemail/snailemailbatch/config/BatchConfig.java
+++ src/main/java/kr/snailemail/snailemailbatch/config/BatchConfig.java
@@ -1,12 +1,28 @@
 package kr.snailemail.snailemailbatch.config;
 
+import kr.snailemail.snailemailbatch.entity.Email;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -17,7 +33,63 @@ public class BatchConfig {
     private final StepBuilderFactory stepBuilderFactory;
     private final EntityManagerFactory entityManagerFactory;
 
+    @Bean
+    public Job exampleJob() throws Exception {
+        return jobBuilderFactory.get("exampleJob")
+                .start(exampleStep()).build();
+    }
 
+    @Bean
+    @JobScope
+    public Step exampleStep() throws Exception {
+        return stepBuilderFactory.get("exampleStep")
+                .<Email, Email> chunk(10)
+                .reader(reader(null))
+                .processor(processor(null))
+                .writer(writer(null))
+                .build();
+    }
 
+    @Bean
+    @JobScope
+    public JpaPagingItemReader<Email> reader(@Value("#{jobParameters[requestDate]}") String requestDate) throws Exception {
+        log.info("==> reader value : " + requestDate);
+        Map<String, Object> parameterValues = new HashMap<>();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse("2022-11-20 21:50:46", fmt);
+        parameterValues.put("sendDate", dateTime);
+
+        return new JpaPagingItemReaderBuilder<Email>()
+                .pageSize(10)
+                .parameterValues(parameterValues)
+                .queryString("SELECT e FROM Email e WHERE e.sendDate >= sendDate")
+                .entityManagerFactory(entityManagerFactory)
+                .name("JpaPagingItemReader")
+                .build();
+    }
+    @Bean
+    @StepScope
+    public ItemProcessor<Email, Email> processor(@Value("#{jobParameters[requestDate]}") String requestDate) throws Exception {
+        return new ItemProcessor<Email, Email>() {
+            @Override
+            public Email process(Email item) throws Exception {
+                log.info("==> processor Email : " + item);
+                log.info("==> processor value : " + requestDate);
+
+                // 가공할 데이터가 있으면 여기서 가공하면 된다.
+                return item;
+            }
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JpaItemWriter<Email> writer(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        log.info("==> writer value: " + requestDate);
+
+        return new JpaItemWriterBuilder<Email>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
 
 }
---
+++ src/main/java/kr/snailemail/snailemailbatch/scheduler/EmailSendScheduler.java
@@ -0,0 +1,51 @@
+package kr.snailemail.snailemailbatch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@EnableScheduling
+@RequiredArgsConstructor
+@Component
+public class EmailSendScheduler {
+
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    @Scheduled(fixedDelay = 30000) // 30000ms == 30s
+    public void startJob() {
+        try {
+            Map<String, JobParameter> jobParameterMap = new HashMap<>();
+            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date time = new Date();
+
+            String time1 = fmt.format(time);
+
+            jobParameterMap.put("requestDate", new JobParameter(time1));
+            JobParameters parameters = new JobParameters(jobParameterMap);
+            jobLauncher.run(job, parameters);
+        } catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (JobRestartException e) {
+            e.printStackTrace();
+        } catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        } catch (JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
